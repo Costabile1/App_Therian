d9a659218dea527490581b1b6975fd0ad3f8144f
Costabile1 <153669661+Costabile1@users.noreply.github.com>
2026-02-02 11:35:45 -0300

    HTML y css

--- src/app.js
+++ src/app.js
@@ -1,27 +1,25 @@
 const express = require("express");
 const path = require("path");
-const expressLayouts = require("express-ejs-layouts");
+
 
 const app = express();
 
 // middlewares básicos
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
 
 // archivos estáticos
 app.use(express.static(path.join(__dirname, "public")));
 
-// EJS
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(expressLayouts);
-app.set("layout", "Menu");
 
 // rutas
-app.get("/", (req, res) => {
-  res.render("PaginaPrincipal");
+app.get(["/", "/home"], (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'Menu.html'));
 });
+
+app.get("/Login" ,( req,res) =>{
+  res.sendFile(path.join(__dirname, 'views', 'Login.html'));
+})
 
 // server
 app.listen(3000, () => {
